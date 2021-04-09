a0a6a5311198b63b0d117f1be3f255bcfd053c50
Pavan <pavangodhani@gmail.com>
2021-04-09 19:09:08 +0530

    Refactoring book class and done testing using xunit

--- C# Fundamentals/Gradebook/src/Gradebook/Book.cs
+++ C# Fundamentals/Gradebook/src/Gradebook/Book.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book
+    public class Book //By default its internal
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
-        List<double> grades;
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(result.High, grade);
+                result.Low = Math.Min(result.Low, grade);
+
+                result.Average += grade;
+
+            }
+
+            result.Average = result.Average / grades.Count;
+
+            return result;
+        }
+
+        private List<double> grades;
+        private string name;
     }
 
 }
--- C# Fundamentals/Gradebook/src/Gradebook/Program.cs
+++ C# Fundamentals/Gradebook/src/Gradebook/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using GradeBook;
+﻿using GradeBook;
 
 namespace Gradebook
 {
@@ -8,37 +6,15 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            // Array in C#
-            // var grades = new double[] { 1.23, 2.23, 5.65, 989.23, 1.50 };
-            // var grades = new[] { 1.23, 2.23, 5.65, 989.23, 1.50 }; //both are same
-            
-            // foreach (var grade in grades)
-            // {
-            //     System.Console.WriteLine(grade);
-            // }
-            
 
-            // List in C#
-
-            // List<double>grades = new List<double>();
-
-            var book = new Book();
+            var book = new Book("Nobita's grade book");
             book.AddGrade(89.1);
+            book.AddGrade(97.53);
+            book.AddGrade(63.25);
+            book.AddGrade(78.78);
+            book.AddGrade(90);
 
-            var grades = new List<double>() { 1.23, 2.23, 5.65, 989.23, 1.50 }; 
-
-            grades.Add(100.100);
-
-            Console.WriteLine(grades.Count);
-
-            foreach (var grade in grades)
-            {
-                System.Console.WriteLine(grade);
-            }
-            
-            // float a = 3.12F;
-            // Console.WriteLine("Hello C# Leaner");
-            // System.Console.WriteLine($"{a}");
+            // book.showStatistics();
         }
     }
 }
