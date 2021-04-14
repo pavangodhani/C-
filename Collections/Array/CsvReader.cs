ebf9917a91f2a35e298aff72dc102cdae6d6c1d9
Pavan <pavangodhani@gmail.com>
2021-04-14 19:17:17 +0530

    Create demo to show how to read data from an external data source into an array

---
+++ Collections/Array/CsvReader.cs
@@ -0,0 +1,45 @@
+using System.IO;
+
+namespace Array
+{
+    class CsvReader
+    {
+        public string csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this.csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstnNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+
+            //For read the txt file...
+            using (var sr = new StreamReader(csvFilePath))
+            {
+                //Read header line...
+                sr.ReadLine();
+
+                for (int i = 0; i < nCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
--- Collections/Array/Program.cs
+++ Collections/Array/Program.cs
@@ -6,6 +6,7 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            /*
             string[] dayOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
             foreach (var day in dayOfWeek)
@@ -21,6 +22,18 @@ namespace Array
             string choosenDay = dayOfWeek[iDay-1];
 
             System.Console.WriteLine($"That day is {choosenDay}");
+            */
+
+            string filePath = @"E:\KLing\C#\Collections\Array\Final.txt";
+
+            var reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstnNCountries(5);
+
+            foreach (var country in countries)
+            {
+                System.Console.WriteLine($"{country.Population} : {country.Name}");
+            }
         }
     }
 }
