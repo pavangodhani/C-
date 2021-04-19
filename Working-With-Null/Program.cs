a9e3bb5c22fe6effad376483013b46ffe89fa01c
Pavan <pavangodhani@gmail.com>
2021-04-19 14:18:03 +0530

    Create 'working with null' directory

---
+++ Working-With-Null/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_With_Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
